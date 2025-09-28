18c5434875f3d2119c2b65ca9e0891141a768f81
willian.silveira_t <willian.silveira_t@ti.growdev.com.br>
2025-09-28 20:36:40 -0300

    feat: configure Docker environment with PostgreSQL for easy setup

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,30 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('+A Educação - Full Stack Web Developer Test')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
